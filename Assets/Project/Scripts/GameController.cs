0b43f76c313d55e1c08110f0cf69122589d003c6
316Douji <511599737@qq.com>
2018-12-14 00:10:23 +0800

    refered packages

--- Assets/Project/Scripts/GameController.cs
+++ Assets/Project/Scripts/GameController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
     public static int LevelNum = 0;
+    public static int checkPoint = 0;
     public static int InkNum = 30;
     public static int TotalInk = 30;
     public static int CurrentTool = 10;

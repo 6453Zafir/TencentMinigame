5ef999f8106443a97e3439d186732182a310eb58
316Douji <511599737@qq.com>
2018-12-14 21:44:32 +0800

    add flower particle, level updated to 3-1

--- Assets/Project/Scripts/BGController.cs
+++ Assets/Project/Scripts/BGController.cs
@@ -3,25 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BGController : MonoBehaviour {
-    private float total_length = 172.8f;
+    /// <summary>
+    /// 每张背景图的宽度*3
+    /// </summary>
+    private float total_length = 86.4f;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        Vector3 bgPosition = this.transform.position;
+        Vector3 cameraPosition = Camera.main.transform.position; // mainCamera是相机的位置
+        bgPosition.y = Camera.main.transform.position.y;
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
         Vector3 bgPosition = this.transform.position;
         Vector3 cameraPosition = Camera.main.transform.position; // mainCamera是相机的位置
+
         if (bgPosition.x + total_length / 2.0f < cameraPosition.x)
         {
             bgPosition.x += total_length;
+            bgPosition.y = Camera.main.transform.position.y + 5;
             this.transform.position = bgPosition;
         }
         if (bgPosition.x - total_length / 2.0f > cameraPosition.x)
         {
             bgPosition.x -= total_length;
+            bgPosition.y = Camera.main.transform.position.y + 5;
             this.transform.position = bgPosition;
         }
     }
---
+++ Assets/Project/Scripts/LV2_1.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LV2_1 : MonoBehaviour {
+    private GameObject Player;
+    private Transform FlowerPos;
+
+    private bool hasParticle = false;
+    private bool FirstCome = true;
+
+    private GameObject FPPrefab;
+    private GameObject currentParticle;
+    // Use this for initialization
+    void Start () {
+        Player = GameObject.FindGameObjectWithTag("Player").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Player.transform.position.x > 140 && Player.transform.position.x < 210)
+        {
+            if (FirstCome)
+            {
+                FPPrefab = Resources.Load("MainFlowerParticle") as GameObject;
+                FlowerPos = transform.Find("MainFlowerPos").transform;
+                FirstCome = false;
+            }
+            if (!hasParticle)
+            {
+                currentParticle = Instantiate(FPPrefab, FlowerPos);
+                currentParticle.transform.SetParent(FlowerPos);
+                currentParticle.transform.localPosition = Vector3.zero;
+                hasParticle = true;
+            }
+        }
+        else {
+            if (hasParticle && currentParticle != null) {
+                Destroy(currentParticle);
+                hasParticle = false;
+            }
+        }
+    }
+}

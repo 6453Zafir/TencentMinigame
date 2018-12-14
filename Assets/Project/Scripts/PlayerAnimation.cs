02ccf5d85fb26f4ad3d6be1db6080ae4b05acc4d
Tian <337101063@qq.com>
2018-12-14 21:55:06 +0800

    人物左右移动，相机

---
+++ Assets/Project/Scripts/PlayerAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour {
+
+    public Sprite[] Sprites;
+    public float framesPerSec;
+    private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rig;
+	// Use this for initialization
+	void Start () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rig = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (rig.velocity.x != 0)
+        {
+            int index = (int)(Time.time * framesPerSec) % Sprites.Length;
+            spriteRenderer.sprite = Sprites[index];
+        }
+	}
+}

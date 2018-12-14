9a7a5f25fdbdc1487642b883eeb6acdf6e21caa3
316Douji <511599737@qq.com>
2018-12-14 17:47:59 +0800

    level 1-3 finished

--- Assets/Packages/Standard Assets/2D/Scripts/Restarter.cs
+++ Assets/Packages/Standard Assets/2D/Scripts/Restarter.cs
@@ -6,12 +6,69 @@ namespace UnityStandardAssets._2D
 {
     public class Restarter : MonoBehaviour
     {
+        private GameObject player;
+        public void Awake()
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.tag == "Player")
             {
-                SceneManager.LoadScene(SceneManager.GetSceneAt(0).name);
+                player.transform.position = GetNewestCheckPoint();
+               // SceneManager.LoadScene(SceneManager.GetSceneAt(0).name);
             }
         }
+
+        public Vector3 GetNewestCheckPoint()
+        {
+            Vector3 pos = Vector3.zero;
+            switch (GameController.LevelNum)
+            {
+                case 0:
+                    {
+                        if (player.transform.position.x > 30)
+                        {
+                            pos = GameObject.Find("/CheckPoints/CP1-1").transform.position;
+                        }
+                        if (player.transform.position.x > 50)
+                        {
+                            pos = GameObject.Find("/CheckPoints/CP1-2").transform.position;
+                        }
+                        if (player.transform.position.x > 126)
+                        {
+                            pos = GameObject.Find("/CheckPoints/CP1-3").transform.position;
+                        }
+                        if (player.transform.position.x > 197)
+                        {
+                            pos = GameObject.Find("/CheckPoints/CP1-4").transform.position;
+                        }
+                        if (player.transform.position.x > 224)
+                        {
+                            pos = GameObject.Find("/CheckPoints/CP1-5").transform.position;
+                        }
+                        break;
+                    }    
+                case 1:
+                    pos = Vector3.zero;
+                    break;
+                case 2:
+                    pos = Vector3.zero;
+                    break;
+                case 3:
+                    pos = Vector3.zero;
+                    break;
+                case 4:
+                    pos = Vector3.zero;
+                    break;
+                default:
+                    pos = Vector3.zero;
+                    break;
+            }
+            return pos;
+        }
     }
+
+
 }
---
+++ Assets/Project/Scripts/LV1_3.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LV1_3 : MonoBehaviour {
+    public float RookCDTime = 3;
+
+    private Transform rockPos;
+    private GameObject Obstacle;
+    private GameObject Target;
+    private GameObject Player;
+    private GameObject RockPrefab;
+    private GameObject currentRock;
+
+    private bool ShouldNewRock = true;
+    private bool isPass = false;
+    private bool isResourceLoad = false;
+    // Use this for initialization
+    void Start () {
+        Player = GameObject.FindGameObjectWithTag("Player").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Player.transform.position.x > 110)
+        {
+            if (!isResourceLoad) {
+                RockPrefab = Resources.Load("RollingRock") as GameObject;
+                rockPos = transform.Find("RockStartPos").transform;
+                Obstacle = transform.Find("Obstacle").gameObject;
+                Target = transform.Find("Target").gameObject;
+                isResourceLoad = true;
+            }
+            if (!isPass) {
+                if (isResourceLoad && ShouldNewRock)
+                {
+                    currentRock = Instantiate(RockPrefab, rockPos);
+                    currentRock.transform.SetParent(rockPos);
+                    currentRock.transform.localPosition = Vector3.zero;
+                    StartCoroutine(CreatRockWithCD(currentRock));
+                }
+                if (Vector3.Distance(currentRock.transform.position, Target.transform.position) < 2.7f)
+                {
+                    StartCoroutine(PassAnimation(1));
+                    isPass = true;
+                }
+            }
+        }
+	}
+
+    IEnumerator CreatRockWithCD(GameObject currentRock) {
+        ShouldNewRock = false;
+        yield return new WaitForSeconds(RookCDTime);
+        Destroy(currentRock);
+        ShouldNewRock = true;
+    }
+
+    IEnumerator PassAnimation(float ObsatcleWaitTime) {
+        Target.GetComponent<Animation>().Play();
+        yield return new WaitForSeconds(ObsatcleWaitTime);
+        Obstacle.GetComponent<Animation>().Play();
+  
+}
+
+}

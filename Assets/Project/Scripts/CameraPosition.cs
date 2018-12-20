b4be45c44d672f794af261957f9ca2a2c5f6f9df
316Douji <511599737@qq.com>
2018-12-21 01:28:08 +0800

    Camera redefined, smooth move, new fonts added

--- Assets/Project/Scripts/CameraPosition.cs
+++ Assets/Project/Scripts/CameraPosition.cs
@@ -3,30 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraPosition : MonoBehaviour {
-
+    public float yOffest = 2.87f;
+    public float xOffest = 5f;
     public float rightDis;//相机右侧范围大小
     public float leftDis;//相机左侧范围大小
-    public float moveSpeed;//人物到达边框后相机跟随速度
+    //public float moveSpeed;//人物到达边框后相机跟随速度
     public GameObject player;
-	// Use this for initialization
-	void Start () {
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    public float smoothTime = 0.6f;  //Smooth Time
+    private Vector2 velocity = new Vector2(2,2);       //Velocity
+                                    // Use this for initialization
+    void Start () {
 
     }
-    private void FixedUpdate()
+
+    void Update()
     {
-        this.transform.position = new Vector3(this.transform.position.x, player.transform.position.y, this.transform.position.z);
-        if (player.transform.position.x - this.transform.position.x >= rightDis)
+        if (transform.position.x - player.transform.position.x <= rightDis)
         {
-            this.transform.position = new Vector3(this.transform.position.x + moveSpeed, player.transform.position.y, this.transform.position.z);
+            this.transform.position =
+            new Vector3(Mathf.SmoothDamp(transform.position.x, player.transform.position.x + xOffest, ref velocity.x, smoothTime), Mathf.SmoothDamp(transform.position.y, player.transform.position.y + yOffest, ref velocity.y, smoothTime), transform.position.z);
+            // this.transform.position = new Vector3(this.transform.position.x + moveSpeed, player.transform.position.y, this.transform.position.z);
+
         }
-        else if (player.transform.position.x - this.transform.position.x <= leftDis)
+        else if (transform.position.x - player.transform.position.x >= leftDis)
         {
-            this.transform.position = new Vector3(this.transform.position.x - moveSpeed, player.transform.position.y, this.transform.position.z);
+            this.transform.position =
+            new Vector3(Mathf.SmoothDamp(transform.position.x, player.transform.position.x + xOffest, ref velocity.x, smoothTime), Mathf.SmoothDamp(transform.position.y, player.transform.position.y + yOffest, ref velocity.y, smoothTime), transform.position.z);
+
+            // this.transform.position = new Vector3(this.transform.position.x - moveSpeed, player.transform.position.y, this.transform.position.z);
+        }
+        else {
+            this.transform.position = new Vector3(player.transform.position.x + xOffest, player.transform.position.y + yOffest, this.transform.position.z);
+
         }
     }
 }
